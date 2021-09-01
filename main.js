669bd819b5939f063072a3fb56c819bed84cc3e7
Agustín López <joellopez011304@gmail.com>
2021-09-01 16:04:08 -0300

    a

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function draw() {
   {
     pintarCasillero(columnaPresionada, filaPresionada, COLOR_CASILLERO_SIN_MINA); //pinta el casillero clickeado. Modificar/completar
       if(tieneMinaCasillero(columnaPresionada, filaPresionada)){
-        perder(); //asdasdadsasd
+        perder();
       } 
     hizoClick = false;  //Indico que ya "procesé" el click del usuario. NO modificar
   }
